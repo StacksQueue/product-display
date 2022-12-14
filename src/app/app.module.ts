1a6cc9404a88f4ec54d18ce01d49657751f71fa0
PaulElyson <paulelyson.villaceran@gmail.com>
2022-12-14 16:37:34 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { AddedProductListComponent } from './components/added-product-list/added-product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    AddedProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/added-product-list/added-product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-added-product-list',
+  templateUrl: './added-product-list.component.html',
+  styleUrls: ['./added-product-list.component.scss']
+})
+export class AddedProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
